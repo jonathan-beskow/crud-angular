46fdb64669ba78083b83cd537458ccc2852e9df0
M373746 <j.beskow@sistran.com.br>
2026-07-10 08:18:14 -0300

    adding list and search function

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatAnchor, MatButtonModule } from "@angular/material/button";
@@ -6,6 +7,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Cliente } from '../cadastro/cliente';
+import { ClienteService } from '../cliente.service';
 @Component({
   selector: 'app-consulta',
   imports: [
@@ -16,9 +20,34 @@ import { MatTableModule } from '@angular/material/table';
     FormsModule,
     MatTableModule,
     MatAnchor,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss',
 })
-export class Consulta { }
+export class Consulta implements OnInit {
+  nomeBusca: string = '';
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"]
+
+  constructor(
+    private service: ClienteService,
+    private router: Router,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
+
+  pesquisar() {
+    this.listaClientes = this.service.pesquisarClientes(this.nomeBusca)
+  }
+
+  preparaEditar(id: string) {
+    this.router.navigate(['/cadastro'], { queryParams: { "id": id } })
+  }
+
+}
